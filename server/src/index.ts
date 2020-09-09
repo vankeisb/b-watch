87d2d325e993daf9f74101d0a0fb5fdb790b212a
Rémi Van Keisbelck <remi@rvkb.com>
2020-09-09 18:06:12 +0200

    tbc

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import * as WebSocket from "ws";
 import * as http from 'http';
 
-import {BuildConfig, CIClient} from "./ciclient/CIClient";
+import {Build, BuildConfig, CIClient} from "./ciclient/CIClient";
 
 // const DTX: BuildConfig = {
 //     tag: "bamboo",
@@ -22,42 +22,59 @@ const intellirule: BuildConfig = {
     }
 };
 
-const ciClient = new CIClient([ intellirule ], build => {
-    console.log("====>>>> build updated", build.uuid, build.status)
-});
-ciClient.list().forEach(b => b.start());
-
 const app = express();
 
 const server = http.createServer(app);
 
-// const wss = new WebSocket.Server({server});
-//
-// wss.on('connection', ws => {
-//
-//     console.log("XXXXXXXXXXXXXXXx")
-//
-//     //connection is up, let's add a simple simple event
-//     ws.on('message', (message: string) => {
-//
-//         //log the received message and send it back to the client
-//         console.log('received: %s', message);
-//         ws.send(`Hello, you sent -> ${message}`);
-//     });
-//
-//     //send immediatly a feedback to the incoming connection
-//     ws.send('Hi there, I am a WebSocket server');
-// })
+const wss = new WebSocket.Server({server});
+
+let sockets: WebSocket[] = [];
+
+wss.on('connection', (ws: WebSocket) => {
+
+    sockets.push(ws);
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+        if (message === "list") {
+
+        }
+
+    });
+
+    //send immediatly a feedback to the incoming connection
+    ws.send('Hi there, I am a WebSocket server');
+
+    ws.on("close", code => {
+        sockets = sockets.filter(s => s !== ws);
+    })
+})
+
+const ciClient = new CIClient([ intellirule ], build => {
+    console.log("====>>>> build updated, notifying " + sockets.length + " client(s)", build.uuid, build.status)
+    sockets.forEach(ws => {
+        ws.send(JSON.stringify(buildToMsg(build)));
+    })
+});
+ciClient.list().forEach(b => b.start());
+
+
+function buildToMsg(build: Build): any {
+    const { uuid, status } = build;
+    return {
+        uuid,
+        status
+    }
+}
+
+function allBuildsToMsg(): any {
+    return {
+        builds: ciClient.list().map(buildToMsg)
+    }
+}
 
 app.use("/", (req, res) => {
-    const builds = ciClient.list().map(b => ({
-      uuid: b.uuid,
-      status: b.status,
-    }))
-    const o = {
-        builds
-    }
-    res.send(JSON.stringify({builds}, null, "  "));
+    res.send(JSON.stringify(allBuildsToMsg(), null, "  "));
 })
 
 server.listen(3000, () => {
