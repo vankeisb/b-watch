fe969be4da5ebe27dcdd6fd016e7446ec94d9e2d
Rémi Van Keisbelck <remi@rvkb.com>
2020-09-10 17:51:29 +0200

    build

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 echo "Yalla"
 
 yarn install && \
+echo "*** common ***" && \
 cd common && yarn build && cd .. && \
+echo "*** daemon ***" && \
 cd daemon && yarn build && cd .. && \
+echo "*** common-front ***" && \
 cd common-front && yarn build && cd .. && \
+echo "*** frontend ***" && \
 cd frontend && yarn build && cd .. && \
-cd electron-app && yarn clean && yarn compile && yarn pack \
+echo "*** electron-app ***" && \
+cd electron-app && yarn clean && yarn compile && yarn dist \
