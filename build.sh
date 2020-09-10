95717c5df1c6c62cffed6749d7e78b70768e465b
Rémi Van Keisbelck <remi@rvkb.com>
2020-09-10 17:39:08 +0200

    build

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ cd common && yarn build && cd .. && \
 cd daemon && yarn build && cd .. && \
 cd common-front && yarn build && cd .. && \
 cd frontend && yarn build && cd .. && \
-cd electron-app && yarn clean && yarn compile && yarn dist \
+cd electron-app && yarn clean && yarn compile && yarn pack \
