04e2c22d9a787532747a2ba4edaf473192ce94b3
Rémi Van Keisbelck <remi@rvkb.com>
2020-09-10 15:23:06 +0200

    icon, tray

--- electron-app/src/electron.ts
+++ electron-app/src/electron.ts
@@ -1,19 +1,60 @@
-import {app, BrowserWindow} from 'electron';
+import {app, BrowserWindow, Menu, Tray} from 'electron';
 import {createServerFromArgs} from "bwatch-daemon";
 import chalk from "chalk";
+import * as path from "path";
 
-function createWindow () {
+function createWindow() {
+    const icons = {
+        'linux': 'iconTemplateWhite.png',
+        'win32': 'windows-icon.png'
+    }
+    const iconName = icons[process.platform] || 'iconTemplate.png'
+    const icon = path.join('assets', 'tray-icon', iconName)
+
     // Create the browser window.
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+        title: "bwatch",
+        icon,
     });
+
+    win.removeMenu();
 
     // and load the index.html of the app.
     win.loadFile('index.html');
+
+    win.on('minimize', event => {
+        event.preventDefault();
+        win.hide();
+    });
+
+    win.on("close", () => {
+        console.log("App closed");
+    });
+
+    app.dock && app.dock.hide();
+    const tray = new Tray(icon)
+    const contextMenu = Menu.buildFromTemplate([
+            {
+                label: 'Show Builds'
+                , click: () => {
+                    win.show();
+                }
+            },
+            {
+                label: 'Quit'
+                , click: () => {
+                    app.quit();
+                }
+            }
+        ]
+    )
+    tray.setToolTip('build-watcher')
+    tray.setContextMenu(contextMenu)
 }
 
 app.on('ready', createWindow);
@@ -35,5 +76,3 @@ switch (server.tag) {
     }
 }
 
-//
-// const ciClient: CIClient = new CIClient()
