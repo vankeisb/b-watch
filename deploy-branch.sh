32b481fcf94c83263105edcc54555a63803ea199
Rémi Van Keisbelck <remi@rvkb.com>
2020-09-18 16:22:26 +0200

    travis externalize scripts

---
+++ deploy-branch.sh
@@ -0,0 +1,11 @@
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  docker run --rm \
+    --env-file bwatch.env \
+    -v ${PWD}:/project \
+    -v ~/.cache/electron:/root/.cache/electron \
+    -v ~/.cache/electron-builder:/root/.cache/electron-builder \
+    electronuserland/builder:wine \
+    /bin/bash -c "./build.sh && cd electron-app && yarn dist --linux --win"
+else
+  /bin/bash -c "./build.sh && cd electron-app && yarn dist --mac"
+fi
---
+++ deploy-tag.sh
@@ -0,0 +1,11 @@
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  docker run --rm \
+    --env-file bwatch.env \
+    -v ${PWD}:/project \
+    -v ~/.cache/electron:/root/.cache/electron \
+    -v ~/.cache/electron-builder:/root/.cache/electron-builder \
+    electronuserland/builder:wine \
+    /bin/bash -c "cd electron-app && yarn deploy --linux --win"
+else
+  /bin/bash -c "cd electron-app && yarn deploy --mac"
+fi
